2fabadac2da3fdbefec68c062d12c710c5f2cc7f
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-03 17:50:10 +0530

    change all id into module css className

--- App.jsx
+++ App.jsx
@@ -3,9 +3,12 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div className={styles.calculator}>
-      <input type="text" />
-      <div id="button-container">
-        <button>C</button>
+      <input type="text" className={styles.display} />
+      <div className={styles.buttonContainer}>
+        <button className={styles.button}>C</button>
+        <button className={styles.button}>1</button>
+        <button className={styles.button}>2</button>
+        <button className={styles.button}>+</button>
       </div>
     </div>
   );
