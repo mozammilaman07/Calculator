5dc4afad494ccc6015fc20ea4b8c145fbfad36bd
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-05 22:02:23 +0530

    add closing tag

--- App.jsx
+++ App.jsx
@@ -4,8 +4,8 @@ import ButtonContainer from "./ButtonContainer";
 function App() {
   return (
     <div className={styles.calculator}>
-      <Display></Display>
-      <ButtonContainer></ButtonContainer>
+      <Display />
+      <ButtonContainer />
     </div>
   );
 }
